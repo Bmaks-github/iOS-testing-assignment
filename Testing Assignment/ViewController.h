ff8c5d6ccb8fd28f5b97d01a325868958e0a564f
Bura Maxim <bmaksmail@gmail.com>
2016-08-24 15:43:27 +0300

    Initial Commit

---
+++ Testing Assignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Testing Assignment
+//
+//  Created by Admin on 24/08/16.
+//  Copyright © 2016 Elantix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Testing Assignment/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Testing Assignment
+//
+//  Created by Admin on 24/08/16.
+//  Copyright © 2016 Elantix. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
